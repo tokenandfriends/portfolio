3a010931cf30c7292a6b00f3b31a33f2d37b23b2
tokenandfriends <tokenandfriends@gmail.com>
2015-12-01 19:46:29 -0500

    new changes
    fixed http.get function for angular

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 angular.module('app', [])
 .controller('gitHubDataController', ['$scope', '$http', function($scope,$http) {
+  'use strict';
   
   $scope.username = "tokenandfriends";
   $http.get('https://api.github.com/users/'+$scope.username)
